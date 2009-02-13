1fc7b174739eebf81a60c4f1993956cbff63a1b0
Maxim Nikolenko <maxim.nikolenko@gmail.com>
2009-02-13 23:15:04 +0200

    all functionality is done, needs refactoring and commenting

--- klondike.js
+++ klondike.js
@@ -1,12 +1,18 @@
 // Make sure that the $ alias referred to the jQuery name in a localized manner
 // without affecting the remainder of the page.
+var DROPPABLE_OPTIONS = {hoverClass: 'highlighted', 
+			 drop: function (event, ui) { 
+			   ui.draggable.card('dropped_on', $(this)); 
+			 }
+			};
+
 (function ($) {
   var Card = {
-    _SUITS: [{symbol: '&diams;', color: 'red'}, 
-	     {symbol: '&spades;', color: 'black'}, 
-	     {symbol: '&hearts;', color: 'red'},
-	     {symbol: '&clubs;', color: 'black'}],
-    _RANKS: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],    
+    _SUITS: [{symbol: 'diams', color: 'red'}, 
+	     {symbol: 'spades', color: 'black'}, 
+	     {symbol: 'hearts', color: 'red'},
+	     {symbol: 'clubs', color: 'black'}],
+    _RANKS: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
     _init: function () {
       var index = this.options.index;
       var num_ranks = this._RANKS.length;
@@ -14,7 +20,7 @@
       this._setData('suit', suit);
       this._setData('rank', index - (suit * num_ranks));
       this.element.addClass('container')
-	.append('<div class="downturned card">');
+	.append('<div class="downturned">');
     },
     _suit: function () { return this._getData('suit'); },
     _rank: function () { return this._getData('rank'); },
@@ -22,17 +28,15 @@
     is_downturned: function () { 
       return this.element.children('.downturned').length != 0; 
     },
-    droppable_enable: function () { 
-      this.element.droppable('enable'); 
-    },
-    _current_scope: function () { 
+    _scope: function () { 
       with (this) {
-	return _SUITS[_suit()].color + _RANKS[_rank()]; 
+	return _SUITS[_suit()].color+_RANKS[_rank()] + ' ' + 
+	       _SUITS[_suit()].symbol+_RANKS[_rank()]; 
       }
     },
-    _acceptable_scope: function () {
+    _tableau_pile_scope: function () {
       with (this) {
-	var scope = '';
+	var scope = '.';
 	scope += (_SUITS[_suit()].color == 'red') ? 'black' : 'red';
 	var rank = _rank();
 	if (rank == 0) {
@@ -47,48 +51,83 @@
 	return scope;	
       }
     },
+    _foundation_scope: function () {
+      with (this) {
+	var scope = '.'+_SUITS[_suit()].symbol;
+	var rank = _rank();
+	if (rank == (_RANKS.length-1)) {
+	  scope += _RANKS[0];
+	} else {
+	  scope += _RANKS[rank+1];
+	}
+	return scope;
+      }
+    },
     upturn: function () {
       with (this) {
 	if (is_downturned()) {
-	  element.children('.card')
+	  element.children('.downturned')
 	    .removeClass('downturned').addClass(_SUITS[_suit()].color)
-	    .append('<span class="suit right">'+_SUITS[_suit()].symbol+'</span>')
+	    .append('<span class="suit right">&'+_SUITS[_suit()].symbol+';</span>')
 	    .append('<span class="rank left">'+_RANKS[_rank()]+'</span>')
-	    .append('<span class="suit left">'+_SUITS[_suit()].symbol+'</span>');
-	  element.addClass(_current_scope())
-	    .draggable({containment: '#field', 
-			revert: 'invalid',
-			revertDuration: 200, 
-			zIndex: 99})
-	    .droppable({hoverClass: 'highlighted',
-			accept: '.'+_acceptable_scope(),
-			drop: function (event, ui) {
-			  var $parent_place = ui.draggable.parent();
-			  if ($parent_place.hasClass('tableau')) {
-			    //$parent_place.droppable( );
-			  } else if ($parent_place.card('is_downturned')) {
-			    ui.draggable.parent().card('upturn');
-			  } else {
-			    ui.draggable.parent().card('droppable_enable');
-			  }			
-			  var $target = $(this);
-			  ui.draggable.css({top: '25px', left: 0});
-			  //var target_z_index = new Number($target.children('.card').css('z-index'));
-			  //ui.draggable.children('.card').css('z-index', target_z_index + 1);
-			  $target.append(ui.draggable);
-			  $target.droppable('disable');
-			} });
+	    .append('<span class="suit left">&'+_SUITS[_suit()].symbol+';</span>');
+	  element.addClass(_scope());
+	  if (element.hasClass('ui-draggable-disabled')) {
+	    element.draggable('enable');
+	  } else {
+	    element.draggable({containment: '#field', 
+			       revert: 'invalid',
+			       revertDuration: 200, 
+			       zIndex: 99});
+	  }
+	  if (! element.hasClass('ui-droppable-disabled')) {
+	    element.droppable($.extend(DROPPABLE_OPTIONS, {accept: _tableau_pile_scope()}));	    
+	  }
 	}
-      }      
+      }
+    },
+    downturn: function () {
+      this.element.children('.red, .black').empty().removeClass('red').removeClass('black').addClass('downturned');
+      this.element.draggable('disable').droppable('disable');
+    },
+    dropped_on: function ($target) {
+      with (this) {
+	var $parent_place = element.parent();
+	if ($parent_place.hasClass('tableau-pile')) {
+	  if ($parent_place.hasClass('ui-droppable-disabled')) {
+	    $parent_place.droppable('enable');
+	  } else {
+	    $parent_place.droppable($.extend(DROPPABLE_OPTIONS, {accept: '.blackK, .redK'}));
+	  }
+	} else if ($parent_place.card('is_downturned')) {
+	  $parent_place.card('upturn');
+	} else {
+	  $parent_place.droppable('enable');
+	}
+	element.css({left: '0px'}); // hack hack hack
+	if ($target.parents('.tableau-pile').length != 0) {
+	  set_top(25);
+	} else {
+	  set_top(0);
+	}
+	$target.append(element);
+	$target.droppable('disable');
+	if ($target.hasClass('foundation') || $target.parents('.foundation').length != 0) {
+	  element.droppable('option', 'accept', _foundation_scope());
+	} else {
+	  element.droppable('option', 'accept', _tableau_pile_scope());
+	}
+	element.droppable('enable');
+      }
     }
   };
   $.widget('ui.card', Card); // create the widget
   $.ui.card.getter = "is_downturned";
+   
 })(jQuery);
 
 var deck = new Array(52);
 var tableau_piles = new Array(7);
-var tableau_piles_images = new Array(7);
 
 $(document).ready(function () {
   for (var i = 0; i < deck.length; i++) {
@@ -104,19 +143,48 @@ $(document).ready(function () {
     tableau_piles[p] = deck.splice(0, p+1);
     tableau_piles[p][tableau_piles[p].length-1].card('upturn'); // upturn the last card in each pile
   }
-  for (p = 0; p < tableau_piles_images.length; p++) {
-    var $tableau_pile_image = $('<div class="tableau">').css('top', '130px').css('left', (p*70)+100+'px')
-				        .css('position', 'absolute').appendTo('#field');
-    tableau_piles_images[p] = $tableau_pile_image;
+
+  var $tableau_piles = $('.tableau-pile');
+  var tableau_pile_tops = new Array($tableau_piles.length);
+  for (p = 0; p < $tableau_piles.length; p++) {
+    $tableau_piles.eq(p).css({top: '140px', left: (p*70)+100+'px'});
+    tableau_pile_tops[p] = $tableau_piles.eq(p);
+  }
+
+  var $foundations = $('.foundation');
+  for (var f = 0; f < $foundations.length; f++) {
+    $foundations.eq(f).css({top: '25px', left: (f*70)+310+'px'});
   }
 		    
-   for (p in tableau_piles) {
-     for (c in tableau_piles[p]) {
-       var card = tableau_piles[p][c];
-       card.card('set_top', 10);
-       tableau_piles_images[p].append(card);
-       tableau_piles_images[p] = card;
-     } 
-   }
+  for (p in tableau_piles) {
+    for (c in tableau_piles[p]) {
+      var card = tableau_piles[p][c];
+      if (c > 0) {
+	card.card('set_top', 10);
+      }
+      tableau_pile_tops[p].append(card);
+      tableau_pile_tops[p] = card;
+    } 
+  }
+  
+  var $waste = $('#waste');
+  var $stock = $('#stock');
+
+  for (c in deck) {
+    $stock.append(deck[c]);
+  }
+  
+  $stock.click(function () {
+    if ($stock.html() == '') {
+      $waste.children('.container').card('downturn').appendTo($stock);
+    } else {
+      $waste.append($stock.children('.container:first').card('upturn').droppable('disable'));
+      if ($stock.html() == '') 
+	$stock.removeClass('downturned');
+    }    
+  });
+  
+  $('.foundation').droppable($.extend(DROPPABLE_OPTIONS, {accept: '.redA, .blackA'}));
+
 });
 
